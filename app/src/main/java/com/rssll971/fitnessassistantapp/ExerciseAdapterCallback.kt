1fa0cbb205a19a2fdbdef93aefb1d6a033daccd8
r-khvstnv <r.khvastunov@gmail.com>
2022-05-25 22:12:56 +0500

    Splitting the project into modules

--- app/src/main/java/com/rssll971/fitnessassistantapp/ExerciseAdapterCallback.kt
+++
@@ -1,8 +0,0 @@
-package com.rssll971.fitnessassistantapp
-
-import com.rssll971.fitnessassistantapp.models.ExerciseModel
-
-interface ExerciseAdapterCallback {
-    fun onItemSelected(position: Int, isNeededAdd: Boolean)
-    fun onItemEditing(exerciseModel: ExerciseModel, isExisted: Boolean)
-}
--- app/src/main/java/com/rssll971/fitnessassistantapp/models/BmiHistoryModel.kt
+++
@@ -1,7 +0,0 @@
-package com.rssll971.fitnessassistantapp.models
-
-data class BmiHistoryModel(val id: Int,
-                           val date: String,
-                           val weight: Float,
-                           val height: Float,
-                           val bmiIndex: Float)
