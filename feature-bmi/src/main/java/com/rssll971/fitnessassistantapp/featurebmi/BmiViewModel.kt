40d261f2766bcbabe0422efe5b0b34b275c80471
r-khvstnv <r.khvastunov@gmail.com>
2022-05-28 15:59:43 +0500

    Added new feature-module
    - Feature-bmi module and it's UI part

---
+++ feature-bmi/src/main/java/com/rssll971/fitnessassistantapp/featurebmi/BmiViewModel.kt
@@ -0,0 +1,7 @@
+package com.rssll971.fitnessassistantapp.featurebmi
+
+import androidx.lifecycle.ViewModel
+
+class BmiViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ feature-bmi/src/main/java/com/rssll971/fitnessassistantapp/featurebmi/CalculationViewModel.kt
@@ -0,0 +1,7 @@
+package com.rssll971.fitnessassistantapp.featurebmi
+
+import androidx.lifecycle.ViewModel
+
+class CalculationViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
