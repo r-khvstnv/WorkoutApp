167faf89f5b7f541eaff63a9495f929fa7a6c9f2
r-khvstnv <r.khvastunov@gmail.com>
2022-06-06 22:51:34 +0500

    Updated FinishFragment (feature-workout)
    UI updates
    Added Deeplink FinishFragment -> OptionStartFragment

--- feature-workout/src/main/java/com/rssll971/fitnessassistantapp/featureworkout/finish/FinishFragment.kt
+++ feature-workout/src/main/java/com/rssll971/fitnessassistantapp/featureworkout/finish/FinishFragment.kt
@@ -1,11 +1,14 @@
 package com.rssll971.fitnessassistantapp.featureworkout.finish
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.rssll971.fitnessassistantapp.featureworkout.databinding.FragmentFinishBinding
+import com.rssll971.fitnessassistantapp.core.R as RCore
 
 
 class FinishFragment : Fragment() {
@@ -18,6 +21,14 @@ class FinishFragment : Fragment() {
     ): View {
         _binding = FragmentFinishBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnQuit.setOnClickListener {
+            val deepLink = Uri.parse(getString(RCore.string.deep_link_option_start_fragment))
+            findNavController().navigate(deepLink)
+        }
     }
 
     override fun onDestroyView() {
