23924c13730194f43a9c50e09283176d649886b5
r-khvstnv <r.khvastunov@gmail.com>
2022-06-08 20:14:59 +0500

    Added LineCharts for statistic (feature-workout-options)

--- feature-workout-options/src/main/java/com/rssll971/fitnessassistantapp/featureworkoutoptions/start/OptionStartFragment.kt
+++ feature-workout-options/src/main/java/com/rssll971/fitnessassistantapp/featureworkoutoptions/start/OptionStartFragment.kt
@@ -11,9 +11,17 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
 import androidx.navigation.fragment.findNavController
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.google.android.material.color.MaterialColors
+import com.rssll971.fitnessassistantapp.core.utils.CoreUtils
 import com.rssll971.fitnessassistantapp.featureworkoutoptions.R
 import com.rssll971.fitnessassistantapp.featureworkoutoptions.databinding.FragmentOptionStartBinding
-import com.rssll971.fitnessassistantapp.featureworkoutoptions.start.di.OptionStartComponent
 import com.rssll971.fitnessassistantapp.featureworkoutoptions.start.di.OptionStartComponentViewModel
 import javax.inject.Inject
 
@@ -44,10 +52,135 @@ class OptionStartFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.llStart.setOnClickListener {
-            findNavController().navigate(
-                R.id.options_nested_graph
-            )
+        with(binding){
+            lineChartWorkoutDuration.setupChartAppearance()
+            lineChartWorkoutAmount.setupChartAppearance()
+
+            llStart.setOnClickListener {
+                findNavController().navigate(
+                    R.id.options_nested_graph
+                )
+            }
+        }
+
+        viewModel.isLineChartShouldBeShown.observe(viewLifecycleOwner){
+            isShouldBeVisible ->
+            if (isShouldBeVisible){
+                binding.llStatistic.visibility = View.VISIBLE
+                binding.tvNoData.visibility = View.GONE
+            } else{
+                binding.llStatistic.visibility = View.GONE
+                binding.tvNoData.visibility = View.VISIBLE
+            }
+        }
+
+        viewModel.workoutDurationEntries.observe(viewLifecycleOwner){
+            list ->
+            list?.let {
+                if (it.isNotEmpty()){
+                    updateDurationLineChart(it, binding.lineChartWorkoutDuration)
+                }
+            }
+        }
+
+        viewModel.workoutExerciseAmountEntries.observe(viewLifecycleOwner){
+                list ->
+            list?.let {
+                if (it.isNotEmpty()){
+                    updateExerciseAmountLineChart(it, binding.lineChartWorkoutAmount)
+                }
+            }
+        }
+    }
+
+
+    private fun LineChart.setupChartAppearance(){
+        setTouchEnabled(false)
+        description.isEnabled = true
+        setDrawGridBackground(false)
+        isDragEnabled = true
+        legend.isEnabled = false
+        description.textColor = MaterialColors.getColor(this@setupChartAppearance, R.attr.colorOnBackground)
+        description.textSize = 18f
+
+        axisRight.isEnabled = false
+        axisRight.setDrawGridLines(false)
+
+        axisLeft.setDrawGridLines(false)
+        axisLeft.isEnabled = false
+
+        xAxis.apply {
+            setDrawLabels(true)
+            setDrawGridLines(false)
+            textColor = MaterialColors.getColor(this@setupChartAppearance, R.attr.colorOnBackground)
+            position = XAxis.XAxisPosition.BOTTOM
+            labelRotationAngle = -90f
+            granularity = 1f
+        }
+
+        xAxis.valueFormatter = object : ValueFormatter(){
+            override fun getFormattedValue(value: Float): String {
+                return viewModel.getDateByIndex(value.toInt())
+            }
+        }
+    }
+
+    private fun updateDurationLineChart(entryList: List<Entry>, lineChart: LineChart){
+        val title = getString(R.string.title_workout_duration)
+        val durationDataSet = LineDataSet(entryList, title)
+        durationDataSet.apply {
+            mode = LineDataSet.Mode.CUBIC_BEZIER
+            setDrawValues(true)
+            valueTextColor = MaterialColors.getColor(lineChart, R.attr.colorPrimary)
+            valueTextSize = 12f
+            setDrawCircles(true)
+            setCircleColor(MaterialColors.getColor(lineChart, R.attr.colorPrimary))
+            color = MaterialColors.getColor(lineChart, R.attr.colorPrimary)
+            lineWidth = 3f
+            setDrawFilled(true)
+            fillColor = MaterialColors.getColor(lineChart, R.attr.colorOutline)
+
+            valueFormatter = object : ValueFormatter(){
+                override fun getFormattedValue(value: Float): String {
+                    return CoreUtils.getFormattedTime(value.toInt() * 1000L)
+                }
+            }
+        }
+        val chartData = LineData(durationDataSet)
+        lineChart.apply {
+            data = chartData
+            description.text = title
+            animateX(1000)
+            invalidate()
+        }
+    }
+    private fun updateExerciseAmountLineChart(entryList: List<Entry>, lineChart: LineChart){
+        val title = getString(R.string.title_exercise_amount)
+        val amountDataSet = LineDataSet(entryList, title)
+        amountDataSet.apply {
+            mode = LineDataSet.Mode.CUBIC_BEZIER
+            setDrawValues(true)
+            valueTextColor = MaterialColors.getColor(lineChart, R.attr.colorSecondary)
+            valueTextSize = 12f
+            setDrawCircles(true)
+            setCircleColor(MaterialColors.getColor(lineChart, R.attr.colorSecondary))
+            color = MaterialColors.getColor(lineChart, R.attr.colorSecondary)
+            lineWidth = 3f
+            setDrawFilled(true)
+            fillColor = MaterialColors.getColor(lineChart, R.attr.colorOutline)
+
+            valueFormatter = object : ValueFormatter(){
+                override fun getFormattedValue(value: Float): String {
+                    return value.toInt().toString()
+                }
+            }
+        }
+        val chartData = LineData(amountDataSet)
+        lineChart.apply {
+            data = chartData
+            description.text = title
+            animateX(1000)
+            invalidate()
         }
     }
 
--- feature-workout-options/src/main/java/com/rssll971/fitnessassistantapp/featureworkoutoptions/start/OptionStartViewModel.kt
+++ feature-workout-options/src/main/java/com/rssll971/fitnessassistantapp/featureworkoutoptions/start/OptionStartViewModel.kt
@@ -1,9 +1,69 @@
 package com.rssll971.fitnessassistantapp.featureworkoutoptions.start
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.rssll971.fitnessassistantapp.core.utils.CoreUtils
 import com.rssll971.fitnessassistantapp.coredata.db.repository.StatisticRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class OptionStartViewModel @Inject constructor(private val repository: StatisticRepository) : ViewModel() {
+    private var _isLineChartShouldBeShown = MutableLiveData(false)
+    val isLineChartShouldBeShown: LiveData<Boolean> get() = _isLineChartShouldBeShown
+    private var _workoutDurationEntries: MutableLiveData<List<Entry>> = MutableLiveData()
+    private var _workoutExerciseAmountEntries: MutableLiveData<List<Entry>> = MutableLiveData()
+    val workoutDurationEntries: LiveData<List<Entry>> get() = _workoutDurationEntries
+    val workoutExerciseAmountEntries: LiveData<List<Entry>> get() = _workoutExerciseAmountEntries
 
+    private var _chartAssociationDateList: MutableLiveData<List<Long>> = MutableLiveData()
+
+    init {
+        fetchData()
+    }
+
+    private fun fetchData(){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.getStatisticList().collect {
+                list ->
+                if (list.isEmpty()){
+                    _isLineChartShouldBeShown.postValue(false)
+                } else{
+                    _isLineChartShouldBeShown.postValue(true)
+                    val durationEntries = arrayListOf<Entry>()
+                    val exerciseAmountEntries = arrayListOf<Entry>()
+                    val associationList = arrayListOf<Long>()
+                    var counter = 10
+
+                    for (i in list.indices){
+                        if (counter != 0){
+                            associationList.add(list[i].date)
+                            durationEntries.add(Entry(i.toFloat(), (list[i].restDuration + list[i].exerciseDuration) * list[i].selectedExerciseIds.size.toFloat()))
+                            exerciseAmountEntries.add(Entry(i.toFloat(), list[i].selectedExerciseIds.size.toFloat()))
+
+                            counter--
+                        } else{
+                            break
+                        }
+                    }
+
+                    _chartAssociationDateList.postValue(associationList.toList())
+                    _workoutDurationEntries.postValue(durationEntries.toList())
+                    _workoutExerciseAmountEntries.postValue(exerciseAmountEntries.toList())
+                }
+            }
+        }
+    }
+
+    fun getDateByIndex(index: Int): String{
+        return _chartAssociationDateList.value?.let {
+            CoreUtils.formatDateToDayMonth(it[index])
+        } ?: ""
+    }
 }
