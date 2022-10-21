33efa1d5b808b905404fa982dcf4d7c5c9b96229
r-khvstnv <r.khvastunov@gmail.com>
2022-10-21 22:14:29 +0500

    Added sources for each db table

---
+++ core-data/src/main/java/com/rssll971/fitnessassistantapp/coredata/data/repository/source/StatisticSource.kt
@@ -0,0 +1,24 @@
+/************************************************
+ * Created by Ruslan Khvastunov                 *
+ * r.khvastunov@gmail.com                       *
+ * Copyright (c) 2022                           *
+ * All rights reserved.                         *
+ *                                              *
+ ************************************************/
+
+
+package com.rssll971.fitnessassistantapp.coredata.data.repository.source
+
+import com.rssll971.fitnessassistantapp.coredata.db.WorkoutDatabase
+import com.rssll971.fitnessassistantapp.coredata.db.dao.StatisticDao
+import com.rssll971.fitnessassistantapp.coredata.db.entity.StatisticEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class StatisticSource @Inject constructor(workoutDatabase: WorkoutDatabase) {
+    private val dao: StatisticDao = workoutDatabase.getStatisticDao()
+    suspend fun insertStatistic(statisticEntity: StatisticEntity){
+        dao.insertStatisticEntity(statisticEntity = statisticEntity)
+    }
+    fun getAllStatistic(): Flow<List<StatisticEntity>> = dao.getAllStatisticEntities()
+}
