fee47e611758080a5130986b5697b24a36b4978b
r-khvstnv <r.khvastunov@gmail.com>
2023-01-03 23:36:56 +0500

    Fixed issue with incorrect interpretation of bmi index

--- feature-bmi/src/main/java/com/rssll971/fitnessassistantapp/featurebmi/utils/Utils.kt
+++ feature-bmi/src/main/java/com/rssll971/fitnessassistantapp/featurebmi/utils/Utils.kt
@@ -37,11 +37,10 @@ internal object Utils {
      * */
     fun getBmiIndexStatus(index: Float, context: Context): String{
         return when(index){
-            in 0.0f..18.5f -> context.getString(R.string.index_underweight)
-            in 18.6f..24.9f -> context.getString(R.string.index_normal)
-            in 25.0f..29.9f -> context.getString(R.string.index_overweight)
-            in 30.0f..34.9f -> context.getString(R.string.index_obese)
-            in 30.0f..34.9f -> context.getString(R.string.index_ex_obese)
+            in 0.0f..18.59f -> context.getString(R.string.index_underweight)
+            in 18.6f..24.99f -> context.getString(R.string.index_normal)
+            in 25.0f..29.99f -> context.getString(R.string.index_overweight)
+            in 30.0f..34.99f -> context.getString(R.string.index_obese)
             else -> context.getString(R.string.index_ex_obese)
         }
     }
@@ -50,11 +49,10 @@ internal object Utils {
      * */
     fun getBmiIndexStatusColor(index: Float, context: Context): Int{
         return when(index){
-            in 0.0f..18.5f -> ContextCompat.getColor(context, R.color.color_underweight)
-            in 18.6f..24.9f -> ContextCompat.getColor(context, R.color.color_normal)
-            in 25.0f..29.9f -> ContextCompat.getColor(context, R.color.color_overweight)
-            in 30.0f..34.9f -> ContextCompat.getColor(context, R.color.color_obese)
-            in 30.0f..34.9f -> ContextCompat.getColor(context, R.color.color_ex_obese)
+            in 0.0f..18.59f -> ContextCompat.getColor(context, R.color.color_underweight)
+            in 18.6f..24.99f -> ContextCompat.getColor(context, R.color.color_normal)
+            in 25.0f..29.99f -> ContextCompat.getColor(context, R.color.color_overweight)
+            in 30.0f..34.99f -> ContextCompat.getColor(context, R.color.color_obese)
             else -> ContextCompat.getColor(context, R.color.color_ex_obese)
         }
     }
